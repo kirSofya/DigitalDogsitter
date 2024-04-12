8f13e3f2ae3c612f281f63e9c27227bc33e3d65b
kirSofya <116517600+kirSofya@users.noreply.github.com>
2024-04-12 15:35:32 +0300

    Update

--- Client/app/src/main/java/com/dog/dogsitter/structs/DataObject.java
+++ Client/app/src/main/java/com/dog/dogsitter/structs/DataObject.java
@@ -4,7 +4,8 @@ import com.yandex.mapkit.map.PlacemarkMapObject;
 
 //класс описывающий данные по объекту на крате
 public class DataObject {
-    public int Category;
+    public int Id, Category;
     public double Lat, Lng;
     public String Description;
+    public boolean Favorite;//является место избранным или нет
 }
